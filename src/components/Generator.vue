067314311ca97b34eba560c4f70c1cf88af1f1dc
József Koller <orosznyet@gmail.com>
2019-12-06 08:49:20 +0100

    Add condition to generator image display.

--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -6,12 +6,11 @@
       Fluctuation: {{ fluctuation }}
       Allow holes: {{ allowHoles }}
     </div>
-    <div id="svg">
-      <svg width="400" height="400" viewBox="0 0 800 400" xmlns="http://www.w3.org/2000/svg">
+    <div class="image">
+      <svg v-if="path" width="400" height="400" viewBox="0 0 800 400" xmlns="http://www.w3.org/2000/svg">
         <g transform="translate(200, 100)">
           <path :d="path" fill="rgba(235, 20, 76, 1)" stroke="none" stroke-width="0" />
         </g>
-        <circle cx="200" cy="300" r="1" stroke="green" fill="transparent" stroke-width="2" />
       </svg>
     </div>
   </div>
@@ -32,6 +31,7 @@ export default {
     const generator = new CloudGenerator(this.width, this.height, this.fluctuation, 10, 2);
     generator.generatePoints();
     generator.generateCutouts();
+
     return {
       path: generator.render()
     }
